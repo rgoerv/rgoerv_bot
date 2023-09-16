2f6b4b0a786c0683f96a6a7ac67efb1a423a3cbd
rgoerv <roma.eremeev.2014@mail.ru>
2023-09-16 18:16:45 +0300

    Initial commin

---
+++ code/common.h
@@ -0,0 +1,166 @@
+#include <tgbot/tgbot.h>
+#include <curl/curl.h>
+
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <ctime>
+
+#include "json.h"
+
+namespace TimeManagement {
+
+struct mini_time {
+    explicit mini_time(std::string_view time /*[operand(1)][hour(2)]:[minute(2)](lenght = 6)*/) {
+        minus = time[0] == '-' ? true : false;
+        hour = static_cast<uint16_t>(std::stoi(static_cast<std::string>(time.substr(1, 2))));
+        minute = static_cast<uint16_t>(std::stoi(static_cast<std::string>(time.substr(4, 2))));
+    }
+
+    uint16_t hour:4;    // [0, 14]
+    uint16_t minute:6;  // [0, 59]
+    bool minus = false;
+};
+
+struct Hasher {
+    size_t operator()(const mini_time& time) const {
+        return std::hash<uint16_t>{}(time.hour) * 37 * 0 +
+                std::hash<uint16_t>{}(time.minute) * 37 * 1 +
+                std::hash<bool>{}(time.minus) * 37 * 2;
+    }
+};
+
+const std::unordered_map<std::string_view, int> wday_to_posw = 
+{
+    {"Mon", 0},
+    {"Tue", 1},
+    {"Wed", 2},
+    {"Thu", 3},
+    {"Fri", 4},
+    {"Sat", 5},
+    {"Sun", 6}
+};
+
+const std::map<int,  mini_time> timezone_utc =
+{
+    {-43200, mini_time{"-12:00"}},
+    {-39600, mini_time{"-11:00"}},
+    {-36000, mini_time{"-10:00"}},
+    {-34200, mini_time{"-09:30"}},
+    {-32400, mini_time{"-09:00"}},
+    {-28800, mini_time{"-08:00"}},
+    {-25200, mini_time{"-07:00"}},
+    {-21600, mini_time{"-06:00"}},
+    {-18000, mini_time{"-05:00"}},
+    {-16200, mini_time{"-04:30"}},
+    {-14400, mini_time{"-04:00"}},
+    {-12600, mini_time{"-03:00"}},
+    {-10800, mini_time{"-03:00"}},
+    {-7200, mini_time{"-02:00"}},
+    {-3600, mini_time{"-01:00"}},
+    {0, mini_time{"00:00"}},
+    {3600, mini_time{"+01:00"}},
+    {7200, mini_time{"+02:00"}},
+    {10800, mini_time{"+03:00"}},
+    {12600, mini_time{"+03:30"}},
+    {14400, mini_time{"+04:00"}},
+    {16200, mini_time{"+04:30"}},
+    {18000, mini_time{"+05:00"}},
+    {19800, mini_time{"+05:30"}},
+    {20700, mini_time{"+05:45"}},
+    {21600, mini_time{"+06:00"}},
+    {23400, mini_time{"+06:30"}},
+    {25200, mini_time{"+07:00"}},
+    {28800, mini_time{"+08:00"}},
+    {32400, mini_time{"+09:00"}},
+    {34200, mini_time{"+09:30"}},
+    {36000, mini_time{"+10:00"}},
+    {37800, mini_time{"+10:30"}},
+    {39600, mini_time{"+11:00"}},
+    {41400, mini_time{"+11:30"}},
+    {43200, mini_time{"+12:00"}},
+    {45900, mini_time{"+12:45"}},
+    {46800, mini_time{"+13:00"}},
+    {50400, mini_time{"+14:00"}}
+};
+
+inline const std::string& GetStringTime(const int timezone) {
+    time_t unix_time = time(NULL);
+    tm* utc_time = gmtime(&unix_time);
+    const mini_time shift_time = timezone_utc.at(timezone);
+
+    if(shift_time.minus) {
+        utc_time->tm_min -= shift_time.minute;
+        utc_time->tm_hour -= shift_time.hour;
+
+        if(utc_time->tm_min < 0) {
+            utc_time->tm_hour -= 1;
+            utc_time->tm_min += 60; 
+        }
+        if(utc_time->tm_hour < 0 ) {
+            utc_time->tm_hour += 24;
+        }
+    }
+    else {
+        utc_time->tm_min += shift_time.minute;
+        utc_time->tm_hour += shift_time.hour;
+
+        if(utc_time->tm_min > 60) {
+            utc_time->tm_hour += 1;
+            utc_time->tm_min -= 60;
+        }
+        if(utc_time->tm_hour > 23) {
+            utc_time->tm_hour -= 24;
+        }
+    }
+
+    const std::string str_time = static_cast<std::string>(asctime(utc_time)).substr(11, 5);
+    return str_time;
+}
+
+} // namespace TimeManagement
+
+namespace Common {
+
+std::unordered_set<std::string> commands = {"start", "time", "weather", "sleep"};
+
+inline bool HandCommand(std::string request_text) {
+    for(const auto& command : commands) {
+        if(!StringTools::startsWith('/' + command, request_text)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+json::Document GetWeatherData(std::string response) {
+    std::istringstream is(response);
+    return json::Load(is);
+}
+
+} // namespace Common
+
+namespace CurlReceiver {
+
+static size_t Writer(char* buffer, size_t size, size_t nmemb, std::string* html) {
+    size_t result = 0;
+    if(buffer != NULL) {
+        html->append(buffer, size * nmemb);
+        result = size * nmemb;
+    }
+    return result;
+}
+
+inline std::string GetResponse(const char* link) {
+    CURL* curl;
+    std::string data;
+    curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_URL, link);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Writer);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+    curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    return data;
+}
+
+} // namepspace CurlReceiver
---
+++ code/telegram_bot.cpp
@@ -0,0 +1,102 @@
+#include "common.h"
+#include "json.h"
+
+#include <stdio.h>
+#include <time.h>
+
+#include <tgbot/tgbot.h>
+
+#include <string>
+#include <string_view>
+#include <iostream>
+#include <locale>
+
+using namespace std::literals;
+
+/* */
+
+const int64_t admin_id = /* admin id */;
+const std::string ERROR_CODE = "404";
+const char BOT_TOKEN[] = /* bot token */;
+bool work_permisson = true;
+
+/* */
+
+// std::string url = "https://api.openweathermap.org/data/2.5/weather?q=Москва&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347";
+
+int main() {
+
+    TgBot::Bot bot(BOT_TOKEN);
+
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi, " + message->chat->firstName + '!');
+    });
+
+    bot.getEvents().onCommand("sleep", [&bot](TgBot::Message::Ptr message){
+        if(message->chat->id == admin_id) {
+            bot.getApi().sendMessage(message->chat->id, "Goodbye! Zzz...");
+            work_permisson = false;
+            return;
+        } else {
+            bot.getApi().sendMessage(message->chat->id, "You don't have authority for perform this command.");
+        }
+    });
+
+    bot.getEvents().onCommand("weather", [&bot](TgBot::Message::Ptr message){
+        std::string_view weather_query = message->text;
+        const std::string weather_str = "/weather";
+
+        if(!(weather_str.size() + 1 < weather_query.size())) {
+            bot.getApi().sendMessage(message->chat->id, "Please, write a city name.");
+            return;
+        }
+
+        std::string city = message->text.substr("/weather"s.size() + 1, message->text.npos);
+        const std::string url = u8"https://api.openweathermap.org/data/2.5/weather?q=" + city 
+                        + u8"&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347";
+
+        const json::Document response = Common::GetWeatherData(CurlReceiver::GetResponse(url.c_str()));
+        const auto& dict_data = response.GetRoot().AsDict();
+
+        if(dict_data.at("cod").IsString() && StringTools::startsWith(dict_data.at("cod").AsString(), ERROR_CODE)) {
+            bot.getApi().sendMessage(message->chat->id, dict_data.at("message").AsString());
+            return;
+        }
+        
+        const std::string temp = std::to_string(static_cast<int>(dict_data.at("main").AsDict().at("temp").AsDouble()));
+        const std::string feels_like = std::to_string(static_cast<int>(dict_data.at("main").AsDict().at("feels_like").AsDouble()));
+        const std::string str_time = TimeManagement::GetStringTime(dict_data.at("timezone").AsInt());
+        
+        bot.getApi().sendMessage(message->chat->id, 
+        ("Right now, weather in " + city + " : " + temp 
+        + "\nFeels like : " + feels_like
+        + "\nTime in this city : " + str_time));
+    });
+
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+
+        printf("User wrote %s\n", message->text.c_str());
+        if(!Common::HandCommand(message->text) || message->text.empty()) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "I don't understand this : " + message->text + ". Please, write correct query.");
+    });
+
+    bot.getEvents().onCommand("lession", [&bot](TgBot::Message::Ptr message) {
+        // сегодня завтра пн вт ср чт пт // текущей недели
+
+    });
+
+    //
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (work_permisson) {
+            longPoll.start();
+        }
+    } catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+
+    return 0;
+}
